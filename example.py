32306066f231e51defd80955333926a70597ac7e
KK-Kris <82802017+KK-Kris@users.noreply.github.com>
2022-11-29 23:20:51 +0800

    Delete example.py

--- example.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-comments = pd.read_csv("688180comments.csv", encoding='GBK')
-print(comments[-50:-22])
-stockdatawithsentiment = pd.read_csv('+-stock_sentiment_data.csv', index_col='date')
-print(stockdatawithsentiment[4:10])
